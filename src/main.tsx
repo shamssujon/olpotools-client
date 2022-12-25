8c5d99fe0d1a47cfd47c190ee8052f0cd76bb83b
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-12-25 08:59:13 +0600

    Remove garbage codes/files, implement Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,31 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { ChakraProvider } from "@chakra-ui/react";
+// Extend theme
+const breakpoints = {
+	sm: "320px",
+	md: "768px",
+	lg: "960px",
+	xl: "1200px",
+	"2xl": "1536px",
+};
+
+// const colors = {
+// 	brand: {
+// 		900: "#1a365d",
+// 		800: "#153e75",
+// 		700: "#2a69ac",
+// 	},
+// };
+
+const theme = extendTheme({ breakpoints });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>
