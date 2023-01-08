6d67122d0d4350089fc309e5b6548cbee8cbac21
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-01-08 10:06:23 +0600

    add coming soon text to Blog page

--- src/components/pages/Blog/Blog.tsx
+++ src/components/pages/Blog/Blog.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { Box, Container, Heading } from "@chakra-ui/react";
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            Blog
-        </div>
-    );
+	return (
+		<Box py={{ base: "12", md: "20" }}>
+			<Container maxW="container.xl">
+				<Box maxW="4xl" mx="auto">
+					<Box textAlign="center">
+						<Heading as="h1" size="2xl">
+							Blog coming soon
+						</Heading>
+					</Box>
+				</Box>
+			</Container>
+		</Box>
+	);
 };
 
 export default Blog;
