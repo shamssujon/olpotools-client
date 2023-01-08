b5221621707363b063105e511fae8e1cdddd1e6b
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-01-08 10:00:47 +0600

    Site text update

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,34 +1,18 @@
-import React from "react";
-import {
-	Box,
-	Container,
-	Flex,
-	Heading,
-	HStack,
-	Icon,
-	IconButton,
-	Image,
-	LinkBox,
-	LinkOverlay,
-	SimpleGrid,
-	Text,
-} from "@chakra-ui/react";
+import { Box, Container, Heading, Icon, LinkBox, LinkOverlay, SimpleGrid, Text } from "@chakra-ui/react";
+import { BsCalendarDate, BsCurrencyExchange } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { BsCurrencyExchange, BsCalendarDate } from "react-icons/bs";
 
 const Hero = () => {
 	return (
-		// <Box pt="20" pb="80" bgGradient="linear(to-b, #6A067A, #2A0845)">
 		<Box py={{ base: "12", md: "20" }}>
 			<Container maxW="container.xl">
 				<Box maxW="4xl" mx="auto">
 					<Box textAlign="center">
 						<Heading as="h1" size="2xl">
-							Convert currency and timezone
+							OLPOTOOLS - makes your life easier
 						</Heading>
-						<Text fontSize="lg" mt="6">
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, vitae nulla. Fugit ducimus
-							tempora.
+						<Text fontSize="xl" mt="6">
+							Olpotools provides various useful tools in one place that we use in our day to day life.
 						</Text>
 					</Box>
 					<SimpleGrid columns={{ base: 1, md: 2 }} spacing="30px" mt="10">
@@ -36,7 +20,7 @@ const Hero = () => {
 							textAlign="center"
 							bg="gray.100"
 							rounded="md"
-							p="6"
+							p="8"
 							_hover={{ boxShadow: "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)" }}
 							transition="all 0.1s">
 							<Box>
@@ -46,24 +30,29 @@ const Hero = () => {
 										Currency Converter
 									</LinkOverlay>
 								</Heading>
-								<Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit vitae nulla.</Text>
+								<Text>
+									Get fast and easy calculator for converting one currency to another using the latest
+									live exchange rates.
+								</Text>
 							</Box>
 						</LinkBox>
 						<LinkBox
 							textAlign="center"
 							bg="gray.100"
 							rounded="md"
-							p="6"
+							p="8"
 							_hover={{ boxShadow: "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)" }}
 							transition="all 0.1s">
 							<Box>
 								<Icon as={BsCalendarDate} fontSize="6xl" mb="4" />
 								<Heading as="h5" size="md" mb="2">
 									<LinkOverlay as={Link} to="/timezone-converter">
-										Timezone Converter
+										Time Converter
 									</LinkOverlay>
 								</Heading>
-								<Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit vitae nulla.</Text>
+								<Text>
+									Find and Compare the current time in different time zones with our time converter.
+								</Text>
 							</Box>
 						</LinkBox>
 					</SimpleGrid>
